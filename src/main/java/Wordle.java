50c59a94c3d51cf00e0376c7e1bc91a5849d2914
DevMattyJS <matej.sliacky@protonmail.com>
2023-10-06 16:05:03 +0200

    Very basic game loop added

--- src/main/java/Wordle.java
+++ src/main/java/Wordle.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Wordle {
 
     private String secretWord;
@@ -9,10 +11,18 @@ public class Wordle {
     }
 
     public void play() {
-
+        Scanner scanner = new Scanner(System.in);
         boolean isGameOver = false;
         while(!isGameOver) {
-            
+            System.out.println("Please, make your guess. Secret word is 5 characters long.");
+            String guess = scanner.nextLine();
+            if (this.secretWord.equalsIgnoreCase(guess)) {
+                System.out.printf("Congratulations, you guessed the secret word: %s.\n", secretWord);
+                isGameOver = true;
+            } else {
+                System.out.println("That's not correct. Try again!");
+                attempts--;
+            }
         }
     }
 }
---
+++ src/main/java/WordleGame.java
@@ -0,0 +1,6 @@
+public class WordleGame {
+    public static void main(String[] args) {
+        Wordle wordle = new Wordle("cream", 6);
+        wordle.play();
+    }
+}
