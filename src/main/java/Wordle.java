a186c11e228213cdaea87f9b95d805f8ec95d575
DevMattyJS <matej.sliacky@protonmail.com>
2023-10-06 15:52:33 +0200

    first commit

---
+++ src/main/java/Wordle.java
@@ -0,0 +1,18 @@
+public class Wordle {
+
+    private String secretWord;
+    private int attempts;
+
+    public Wordle(String secretWord, int attempts) {
+        this.secretWord = secretWord;
+        this.attempts = attempts;
+    }
+
+    public void play() {
+
+        boolean isGameOver = false;
+        while(!isGameOver) {
+            
+        }
+    }
+}
